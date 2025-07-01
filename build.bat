5cef4789f11b4192cf65801c97445421fe2ae3b4
Altire <altire.dev@gmail.com>
2025-07-01 16:01:39 +0100

    BVN

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 pyinstaller ^
     --noupx^
     -F --noconsole --clean ^
-    -n TextureForge-v0.0.1^
+    -n TextureForge-v1.0.0^
     --upx-dir=..\resources\upx-4.2.4-win64^
     --icon=.\textureforge\resources\icon.ico^
     --add-data textureforge\resources\icon.ico:resources^
