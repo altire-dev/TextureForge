e46ac7e92e0c417bed383b6109653d9533c5af1b
Altire <altire.dev@gmail.com>
2025-10-24 11:04:08 +0100

    Fixed upx path

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ pyinstaller ^
     --noupx^
     -F --noconsole --clean ^
     -n TextureForge-v1.4.0^
-    --upx-dir=..\resources\upx-4.2.4-win64^
+    --upx-dir=.\resources\upx-4.2.4-win64^
     --icon=.\textureforge\resources\icon.ico^
     --add-data textureforge\resources\icon.ico:resources^
     --add-data textureforge\bin\texconv.exe:bin^
